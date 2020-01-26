87244cccdf04c3beaeebd6847eef9d67ec65905d
Drew Page <drew@siegemedia.com>
2020-01-26 09:46:14 -0800

    added category tag and modified header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -41,25 +41,22 @@ class Header extends React.Component {
           title={
             <span className="submenu-title-wrapper">
               <Icon type="setting" />
-              Navigation Three - Submenu
+              Categories
             </span>
           }
         >
-          <Menu.ItemGroup title="Item 1">
-            <Menu.Item key="setting:1">Option 1</Menu.Item>
-            <Menu.Item key="setting:2">Option 2</Menu.Item>
+          <Menu.ItemGroup title="Quotes">
+            <Menu.Item key="setting:1">Motivational Quotes</Menu.Item>
+            <Menu.Item key="setting:2">Entrepreneurial Quotes</Menu.Item>
+            <Menu.Item key="setting:3">Love Quotes</Menu.Item>
           </Menu.ItemGroup>
-          <Menu.ItemGroup title="Item 2">
+          <Menu.ItemGroup title="Videos and Podcasts">
             <Menu.Item key="setting:3">Option 3</Menu.Item>
             <Menu.Item key="setting:4">Option 4</Menu.Item>
           </Menu.ItemGroup>
         </SubMenu>
-        <Menu.Item key="alipay">
-          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-            Navigation Four - Link
-          </a>
-        </Menu.Item>
         <Menu.Item>
+        </Menu.Item>
         <InputGroup compact>
           <Select defaultValue="Sign Up">
             <Option value="Sign Up">Sign Up</Option>
@@ -71,7 +68,6 @@ class Header extends React.Component {
             placeholder="email"
           />
         </InputGroup>
-        </Menu.Item>
       </Menu>
     );
   }
